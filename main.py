0fd589c7cdd53443df5ca6bd4aa9bef36ac8e205
GUY <guy1998@gmail.com>
2025-04-15 09:48:24 +0300

    10

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from CourseTrackerUI import CourseTrackerUI
 from CourseDatabase import CourseDatabase
 
-# שלב ראשון - יצירת בסיס הנתונים והוספת דוגמה אחת (אפשר למחוק)
-db = CourseDatabase()
-db.add_course("מבוא למדעי המחשב", "204000", 3.5)
-db.add_course("חדו\"א 1", "201001", 5)
-db.close()
 
 # שלב שני - הרצת ממשק המשתמש
 tracker = CourseTrackerUI()
