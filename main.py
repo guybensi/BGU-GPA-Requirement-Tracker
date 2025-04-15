3343ed94db4935d3bec4a899854f9942f09a4e98
GUY <guy1998@gmail.com>
2025-04-15 11:39:52 +0300

    22

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from CourseDatabase import CourseDatabase
+
+# Initialize DB with courses (only creates table and inserts if needed)
+CourseDatabase()  # Just running this will create/populate the table if not already
+
 from CourseTrackerUI import CourseTrackerUI
 
-# הרצת ממשק המשתמש
 if __name__ == "__main__":
     tracker = CourseTrackerUI()
     tracker.course_input_form()
